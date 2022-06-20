31765a333c62ae623392683d302f3ff2d5e86897
N Khosa <99992430+NKKhosa@users.noreply.github.com>
2022-06-19 23:40:23 -0400

    Adding T-Test analysis and results.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,6 +11,18 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 # 2
 suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F) #import and read the the data file
 
-total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Varaince=var(PSI), SD=sd(PSI), .groups = 'keep') #create a summary table for stats
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create a summary table for stats
 
-lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median=median(PSI), Varaince=var(PSI), SD=sd(PSI), .groups = 'keep') #create a summary table for stats grouped by lot
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median=median(PSI), Variannce=var(PSI), SD=sd(PSI), .groups = 'keep') #create a summary table for stats grouped by lot
+
+# 3
+t.test(suspension_coil$PSI, mu=1500) #compare the sample versus population means
+
+lot1 <- subset(suspension_coil, Manufacturing_Lot == "Lot1") # create a filtered table for lot 1
+t.test(lot1$PSI, mu=1500) #compare Lot1 versus population means
+
+lot2 <- subset(suspension_coil, Manufacturing_Lot == "Lot2") # create a filtered table for lot2
+t.test(lot2$PSI, mu=1500) #compare Lot2 versus population means
+
+lot3 <- subset(suspension_coil, Manufacturing_Lot == "Lot3") #create a filtered table for lot3
+t.test(lot3$PSI, mu=1500) #compare Lot3 versus population means
